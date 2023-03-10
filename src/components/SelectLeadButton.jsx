d1d23f6050ca2d472fa353657cb568a640bfaca4
Edilma Riano <edilmaria@hotmail.com>
2023-03-09 22:24:43 -0500

    STYLED the buttons for buyer

--- src/components/SelectLeadButton.jsx
+++ src/components/SelectLeadButton.jsx
@@ -1,35 +1,36 @@
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
+import "../styles/buyer.css"
 
 
 export default function SelectLeadButton({type}){
     const navigate = useNavigate();
 
     return(
-       <>
+       <div className='button-buyer-page'>
             <Button 
-            className="local-leads-button" 
+            className="leads-button local" 
             size="lg"
             onClick= {()=>navigate(`/leads/local`)}
             >
           Local Leads
         </Button>
         <Button 
-        className="remote-leads-button" 
+        className="leads-button remote" 
         size="lg"
         onClick= {()=>navigate(`/leads/remote`)}
         >
           Remote Leads
         </Button>
         <Button 
-        className="all-leads-button" 
+        className="leads-button all" 
         size="lg"
         onClick={()=>navigate(`/leads/`)}
         >
           All Leads
         </Button>
-        </>
+        </div>
 
     )
 }
